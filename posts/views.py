953a9a9e497f2aeb56ee4a1afe06af74bc77f4bc
fortnite2incel <abdieva.eszett@gmail.com>
2026-05-29 20:15:14 +0600

    first commit

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def hello_world(request):
+
+    return HttpResponse("Hello World!")
+
+def about(request):
+
+    return render(request, "about.html")
